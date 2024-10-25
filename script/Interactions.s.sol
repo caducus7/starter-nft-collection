f4ce40b442a9a166f0f5cb60cee07458639206be
Caducus <stefanoskissas@hotmail.com>
2024-10-25 14:11:04 +0300

    more interactions

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,8 +5,9 @@ import {DeployBasicNft} from "./DeployBasicNft.s.sol";
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "src/BasicNft.sol";
+import {MoodNft} from "src/MoodNft.sol";
 
-contract Interactions is Script {
+contract MintBasicNft is Script {
     string public constant DOG_URI =
         "ipfs://bafybeicz57su3zyafexgchnfqej7zuxfa3e4xgz22yzmge6nzkhfmskqs4.ipfs.localhost:8080/?filename=st-bernard.png";
 
@@ -20,6 +21,32 @@ contract Interactions is Script {
         BasicNft(contractAddress).mintNft(DOG_URI);
         vm.stopBroadcast();
     }
+}
 
-    function MintMoodNft() public {}
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("DeployMoodNft", block.chainid);
+        mintMoodNft(mostRecentlyDeployed);
+    }
+
+    function mintMoodNft(address contractAddress) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    uint256 public constant NFT_TOKEN_ID = 0;
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("DeployMoodNft", block.chainid);
+        flipMoodNft(mostRecentlyDeployed);
+    }
+
+    function flipMoodNft(address contractAddress) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).flipMood(NFT_TOKEN_ID);
+        vm.stopBroadcast();
+    }
 }
